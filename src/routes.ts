e33706a0088de350e2578f9d1c57e0ded95b4e89
vinnidias <vinicius.3991@gmail.com>
2024-03-18 12:19:46 -0300

    rotas incias para criacao, listagem e unica noticia

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,9 @@ import {
   FastifyRequest,
   FastifyReply,
 } from "fastify";
+import { CreateNewsController } from "./controllers/CreateNewsController";
+import { ListNewsController } from "./controllers/ListNewsController";
+import { FindNewsByIdController } from "./controllers/FindNewsByIdController";
 
 export async function routes(
   fastify: FastifyInstance,
@@ -15,6 +18,27 @@ export async function routes(
       return {
         ok: true,
       };
+    }
+  );
+
+  fastify.post(
+    "/news",
+    async (request: FastifyRequest, reply: FastifyReply) => {
+      return new CreateNewsController().handle(request, reply);
+    }
+  );
+
+  fastify.get(
+    "/news",
+    async (request: FastifyRequest, reply: FastifyReply) => {
+      return new ListNewsController().handle(request, reply);
+    }
+  );
+
+  fastify.get(
+    "/newsbyid",
+    async (request: FastifyRequest, reply: FastifyReply) => {
+      return new FindNewsByIdController().handle(request, reply);
     }
   );
 }
