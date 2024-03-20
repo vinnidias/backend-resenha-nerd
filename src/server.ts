5964bec257995059db4f6b5d72609e2c1789ed1e
vinnidias <vinicius.3991@gmail.com>
2024-03-20 01:55:46 -0300

    cnfiguracao de host

--- src/server.ts
+++ src/server.ts
@@ -4,14 +4,15 @@ import { routes } from "./routes";
 
 const app = Fastify({logger: true});
 
-const port = process.env.PORT ? Number(process.env.PORT) : 3333
+const PORT = process.env.PORT ? Number(process.env.PORT) : 3333
+const HOST = '0.0.0.0'
 
 const start = async () => {
   await app.register(cors)
   await app.register(routes)
 
   try {
-    await app.listen({ port: port})
+    await app.listen(PORT, HOST)
   } catch (error) {
     process.exit(1)
   }
