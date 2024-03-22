e3998ee9bbbaff738e18e18c92c84b9ad1484bb7
vinnidias <vinicius.3991@gmail.com>
2024-03-22 18:09:22 -0300

    ajuste de indentacao

--- src/routes.ts
+++ src/routes.ts
@@ -7,19 +7,17 @@ import {
 import { CreateNewsController } from "./controllers/CreateNewsController";
 import { ListNewsController } from "./controllers/ListNewsController";
 import { FindNewsByIdController } from "./controllers/FindNewsByIdController";
+import { CreateAuthorController } from "./controllers/CreateAuthorController";
 
 export async function routes(
   fastify: FastifyInstance,
   options: FastifyInstance
 ) {
-  fastify.get(
-    "/",
-    async (request: FastifyRequest, reply: FastifyReply) => {
-      return {
-        ok: true,
-      };
-    }
-  );
+  fastify.get("/", async (request: FastifyRequest, reply: FastifyReply) => {
+    return {
+      ok: true,
+    };
+  });
 
   fastify.post(
     "/news",
@@ -28,12 +26,13 @@ export async function routes(
     }
   );
 
-  fastify.get(
-    "/news",
-    async (request: FastifyRequest, reply: FastifyReply) => {
-      return new ListNewsController().handle(request, reply);
-    }
-  );
+  fastify.post("/author", (request: FastifyRequest, reply: FastifyReply) => {
+    return new CreateAuthorController().handle(request, reply);
+  });
+
+  fastify.get("/news", async (request: FastifyRequest, reply: FastifyReply) => {
+    return new ListNewsController().handle(request, reply);
+  });
 
   fastify.get(
     "/newsbyid",
