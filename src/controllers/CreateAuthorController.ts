778ffa3b6eaef160ef2eb9c9178eb73fa1e0d107
vinnidias <vinicius.3991@gmail.com>
2024-03-22 18:10:06 -0300

    controller e service de criacao de autor

---
+++ src/controllers/CreateAuthorController.ts
@@ -0,0 +1,23 @@
+import { FastifyRequest, FastifyReply } from "fastify";
+import { CreateAuthorService } from "../services/CreateAuthorService";
+
+interface CreateAuthorProps {
+  name: string;
+  nickname: string;
+  email: string;
+  image?: string;
+}
+
+class CreateAuthorController {
+  async handle(request: FastifyRequest, reply: FastifyReply) {
+    const { name, nickname, email, image } = request.body as CreateAuthorProps;
+
+    const authorService = new CreateAuthorService();
+
+    const author = authorService.execute({ name, nickname, email, image });
+
+    reply.send(author);
+  }
+}
+
+export { CreateAuthorController };
---
+++ src/services/CreateAuthorService.ts
@@ -0,0 +1,28 @@
+import prismaClient from "../prisma";
+
+interface CreateAuthorProps {
+  name: string;
+  nickname: string;
+  email: string;
+  image?: string;
+}
+
+class CreateAuthorService {
+  async execute({ name, nickname, email, image }: CreateAuthorProps) {
+    if (!name || !nickname || !email) {
+      throw new Error("Preencha todos os campos para criar o autor!");
+    }
+    const author = prismaClient.author.create({
+      data: {
+        name,
+        nickname,
+        email,
+        image,
+      },
+    });
+
+    return author;
+  }
+}
+
+export { CreateAuthorService };
