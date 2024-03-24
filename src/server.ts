df10ee376ba246430540395c73838bf4fd605c26
vinnidias <vinicius.3991@gmail.com>
2024-03-24 12:40:26 -0300

    adicionando midlleware de erro

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,14 @@ import Fastify from "fastify";
 import cors from "@fastify/cors";
 import { routes } from "./routes";
 
-const app = Fastify({ logger: true });
-
 const PORT = process.env.PORT ? Number(process.env.PORT) : 3333;
 const HOST = "0.0.0.0";
+
+const app = Fastify({ logger: true });
+
+app.setErrorHandler((error, request, reply) => {
+  reply.code(400).send({ message: error.message });
+});
 
 const start = async () => {
   await app.register(cors, {
