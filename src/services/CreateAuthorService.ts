9ec92b74022922da63e3c670053d571365f73d26
vinnidias <vinicius.3991@gmail.com>
2024-04-04 12:49:46 -0300

    adicionando validacao de apelido e email

--- src/services/CreateAuthorService.ts
+++ src/services/CreateAuthorService.ts
@@ -12,6 +12,28 @@ class CreateAuthorService {
     if (!name || !nickname || !email) {
       throw new Error("Preencha todos os campos para criar o autor!");
     }
+
+    const isUniqueNickName = await prismaClient.author.findFirst({
+      where: {
+        nickname: nickname,
+      },
+    });
+
+    const isUniqueEmail = await prismaClient.author.findFirst({
+      where: {
+        email: email,
+      },
+    });
+
+    if (isUniqueNickName) {
+      console.log("apelido existente: ", isUniqueNickName);
+      throw new Error("Apelido já cadastrado, escolha outro e tente novamente");
+    }
+
+    if (isUniqueEmail) {
+      throw new Error("Email já cadastrado, escolha outro e tente novamente");
+    }
+
     const author = await prismaClient.author.create({
       data: {
         name,
